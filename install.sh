7081ef68a880eeb2b0a9c181d7fd72dd15ba7c65
Felipe Macias <felipe.macias@freetechsolutions.com.ar>
2019-06-18 16:56:55 -0300

    cambio install.sh para hacer cambio de makefile y asterisk.exports.in

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 if [ ! -s include/asterisk.h ] ; then
-	echo "please cd into the directory where the asterisk source has been untarred"
-	exit
+        echo "please cd into the directory where the asterisk source has been untarred"
+        exit
 fi
 cp asterisk-res_json/res_json.c addons/.
 cp asterisk-res_json/cJSON.h include/asterisk/.
 cp asterisk-res_json/cJSON.c addons/.
-echo "edit addons/Makefile: add res_json to the list of modules built, and"
+echo "editing addons/Makefile: add res_json to the list of modules built, and"
 echo "    res_json.so: cJSON.o res_json.o"
+echo "res_json.so: cJSON.o res_json.o" >> addons/Makefile
+sed -i '36i\            res_json \\' addons/Makefile
+echo "editing main/asterisk.exports.in:"
+sed -i '44i\                LINKER_SYMBOL_PREFIXcJSON_*; \' main/asterisk.exports.in
